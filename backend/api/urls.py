914411adfde475f296d4459776de00eb6fc0e545
kolyas3000 <uguaga2004@gmail.com>
2024-07-17 23:36:01 +1000

    customized fields of access token

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
+from rest_framework_simplejwt.views import TokenRefreshView
+
+from .views import CustomTokenObtainPairView
 
 urlpatterns = [
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
